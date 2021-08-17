9e5bacc66bf3508e0a05d719314acc28426f1f8c
Harsh Singh <pinkisingh50229@gmail.com>
2021-08-17 18:56:56 +0530

    add

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +21,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    constrainedLog = new Log(280,180,PI/2);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,6 +39,17 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    var options = {
+        bodyA : bird.body,
+        bodyB : constrainedLog.body,
+        length : 10,
+        stiffness : 0.04
+
+    }
+    var chain = Constraint.create(options);
+    World.add(world,chain);
+
 
 }
 
@@ -61,4 +76,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    strokeWeight(3);
+    line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,
+        constrainedLog.body.position.y);
 }
